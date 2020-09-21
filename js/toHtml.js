8081dcac02e7e9ae76cf3293501bd0736384e695
Dmitry <rinini9009@gmail.com>
2020-09-21 21:17:42 +0300

    fix

--- js/toHtml.js
+++ js/toHtml.js
@@ -29,7 +29,7 @@ function toHtml(Arr) {
     });
     let div = document.createElement('div');
     div.classList.add('item');
-    div.innerHTML = `<div class="item__hero-icon"></div>
+    div.innerHTML = `<img class="item__hero-icon"></img>
     <div class="item__stats">
       <div class="item__kda">
       <table class="item__table">
